845d22318325ad24d5d385cef03fa0b58f1ab75a
1917kun <anleader911@gmail.com>
2020-05-19 10:57:02 +0800

    upload

---
+++ src/components/Marquee.vue
@@ -0,0 +1,4 @@
+<template lang="pug">
+  #Marquee
+    marquee 大家來購物精品網站 真的好累好累
+</template>
---
+++ src/components/Top.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  #top
+    | 流行精品購物網站
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
